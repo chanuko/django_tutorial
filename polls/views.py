bba637039609eaa37b1e774f20aab19bb63420c0
chanuko <teacatus@gmail.com>
2017-12-16 16:53:00 +0900

    もっとビューを書いてみる

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import HttpResponse
 
+
 # Create your views here.
 
 
 def index(request):
     return HttpResponse("Hello World. You're at the polls index.")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
