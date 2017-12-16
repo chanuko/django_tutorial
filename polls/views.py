f13256914654211464564e2890d84fa18a21f3ea
chanuko <teacatus@gmail.com>
2017-12-16 18:14:34 +0900

    404 エラーの送出

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
+import pkg_resources
 from django.shortcuts import HttpResponse
 from django.shortcuts import render
+from django.http import Http404
 from .models import Question
 
 
@@ -15,7 +17,11 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
