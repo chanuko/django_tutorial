2d2ea96097d3cdd7809a36438ee601219738ef4e
chanuko <teacatus@gmail.com>
2017-12-17 09:30:29 +0900

    汎用ビューを使う: コードが少ないのはいいことだ

--- polls/urls.py
+++ polls/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
     # ex: /polls/5/
-    path('specifics/<int:question_id>/', views.detail, name='detail'),
+    path('specifics/<int:pk>/', views.IndexView.as_view(), name='detail'),
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultView.as_view(), name='results'),
     # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:pk>/vote/', views.vote, name='vote'),
 ]
